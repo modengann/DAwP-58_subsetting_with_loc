e788fb6862904d042ed7d32c44136fc0693dc6d9
modengann <99417989+modengann@users.noreply.github.com>
2022-11-17 09:24:31 -0500

    Update subsetting_with_loc.py

--- src/subsetting_with_loc.py
+++ src/subsetting_with_loc.py
@@ -2,11 +2,12 @@
 
 import pandas as pd
 
-def subsetting_with_loc():
+def subsetting_with_loc(data):
     pass
 
 def main():
-    subsetting_with_loc()
+    df = pd.read_csv('src/municipal.tsv', sep='\t', index_col=0)
+    subsetting_with_loc(df)
 
 if __name__ == "__main__":
     main()
